2749f2fc402a3289eed977fa2cdce5c631f753f1
11249a033-dot <11249a033@kanchiuniv.ac.in>
2026-08-05 10:55:07 +0530

    Add files via upload

---
+++ StaticDemo.java
@@ -0,0 +1,9 @@
+class StaticDemo {
+    static void display() {
+        System.out.println("This is a static method");
+    }
+    public static void main(String[] args) {
+        StaticDemo.display();
+    }
+}
+
