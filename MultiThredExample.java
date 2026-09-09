b6c739fde27c82b8fe12fdaf78b0652f44658c71
11249a033-dot <11249a033@kanchiuniv.ac.in>
2026-09-09 11:50:18 +0530

    Add files via upload

---
+++ MultiThredExample.java
@@ -0,0 +1,22 @@
+class FirstThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++){
+            System.out.println("First Thread:" + i);
+        }
+    }
+}
+class SecondThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++){
+            System.out.println("Second Thread:" + i);
+        }
+    }
+}
+public class MultiThredExample {
+    public static void main(String[] args) {
+        FirstThread t1 = new FirstThread();
+        SecondThread t2 = new SecondThread();
+        t1.start();
+        t2.start();
+    }
+}
